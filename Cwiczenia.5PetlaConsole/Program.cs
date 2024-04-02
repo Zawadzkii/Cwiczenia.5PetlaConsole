926fe2bdc31d676115fb27f24a818ec8b105bc9a
Wojciech Zawadzki <zawadzki1993@hotmail.com>
2024-04-02 20:25:54 +0200

    Add project files.

---
+++ Cwiczenia.5PetlaConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		for (int i = 1; i <= 20; i++)
+		{
+			int potega = i * i * i;
+			Console.WriteLine($"{i} do potęgi 3 = {potega}");
+		}
+	}
+}
